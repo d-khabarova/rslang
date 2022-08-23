d5359ebe5537336942c3e8f746f4d7abaee40cb8
Darya Petina <darya.khabarova@gmail.com>
2022-08-23 17:46:43 +0300

    fix: removed unnecessary functions from api methods

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,17 @@
 import { ApiUsers, ApiSignIn } from '../types/apiTypes';
-import StorageController from '../component/storage/storage';
-import { setErrorMessage, renderLogOutBnt } from '../component/authorization/functions';
+import { setErrorMessage } from '../component/authorization/functions';
 
 class API {
-  location: string;
-  storage: StorageController;
+  base: string;
+  users: string;
 
   constructor() {
-    this.location = 'https://react-rslang-be-d-khabarova.herokuapp.com/';
-    this.storage = new StorageController();
+    this.base = 'https://react-rslang-be-d-khabarova.herokuapp.com';
+    this.users = `${this.base}/users`;
   }
 
   async createUser(user: ApiUsers) {
-    const rawResponse = await fetch(`${this.location}users`, {
+    const rawResponse = await fetch(this.users, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -24,12 +23,11 @@ class API {
       setErrorMessage('Пользователь с таким email уже существует');
     }
     const content = await rawResponse.json();
-    this.storage.parseToLocalStroage(content);
-    this.loginUser(user);
+    return content;
   }
 
   async loginUser(user: ApiSignIn) {
-    const rawResponse = await fetch(`${this.location}signin`, {
+    const rawResponse = await fetch(`${this.base}/signin`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -41,8 +39,7 @@ class API {
       setErrorMessage('Неверный email или пароль!');
     }
     const content = await rawResponse.json();
-    this.storage.parseToLocalStroage(content);
-    renderLogOutBnt();
+    return content;
   }
 }
 export default API;
